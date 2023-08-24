c7ecebd980b255fa59d4e59f37d9e43a61fe0d55
gabriele-andolina <gabe.andolina@gmail.com>
2023-08-24 18:07:47 +0200

    Components: Added paragraph to Intro

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ function App() {
     return (
         <div className="card">
             <Avatar />
+            <div className="data">
+                <Intro />
+            </div>
         </div>
     );
 }
@@ -14,7 +17,15 @@ function Avatar() {
     return <img src="images/profile.jpg" className="avatar"></img>;
 }
 
-function Intro() {}
+function Intro() {
+    return (
+        <p>
+            I'm a passionate junior web developer with knowledge of HTML, CSS,
+            JavaScript, Python and Django. Currently studying React and planning
+            two custom projects with the Django REST framework.
+        </p>
+    );
+}
 
 function SkillList() {}
 
