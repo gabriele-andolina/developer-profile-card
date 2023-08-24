b285045a3435e67ab65c15019e7f59120bfee9a3
gabriele-andolina <gabe.andolina@gmail.com>
2023-08-24 17:58:40 +0200

    Components: Created Avatar, Intro, and SkillList empty functions

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ function App() {
     return <div></div>;
 }
 
+function Avatar() {}
+
+function Intro() {}
+
+function SkillList() {}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
